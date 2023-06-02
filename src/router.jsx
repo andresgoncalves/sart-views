661feeff0329b7b9e24524e6245b775a3a36c319
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-02 18:14:16 -0400

    Changed router to Route element tree

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import BlankPage from "./views/BlankPage";
 import ErrorPage from "./views/ErrorPage";
 import HomePage from "./views/HomePage";
 
-export default createBrowserRouter([
-  { path: "/", Component: HomePage, ErrorBoundary: ErrorPage },
-  { path: "/login", Component: BlankPage },
-  { path: "/registro", Component: BlankPage },
-  { path: "/obras", Component: BlankPage },
-  { path: "/tours", Component: BlankPage },
-  { path: "/calendario", Component: BlankPage },
-]);
+export default createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<ErrorPage />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<BlankPage />} />
+      <Route path="/registro" element={<BlankPage />} />
+      <Route path="/obras" element={<BlankPage />} />
+      <Route path="/tours" element={<BlankPage />} />
+      <Route path="/calendario" element={<BlankPage />} />
+    </Route>
+  )
+);
