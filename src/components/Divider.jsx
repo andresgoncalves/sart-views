bca8dc24f9243f2f8135611b9ca910a9162516d8
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-03 22:10:47 -0400

    Added Divider component

---
+++ src/components/Divider.jsx
@@ -0,0 +1,22 @@
+import styles from "./Divider.module.scss";
+
+/**
+ * @typedef {{
+ *   children?: React.ReactNode;
+ * }} DividerProps
+ */
+
+/** @param {DividerProps} props */
+export default function Divider({ children }) {
+  return children ? (
+    <div className={styles.divider}>
+      <hr />
+      {children}
+      <hr />
+    </div>
+  ) : (
+    <div>
+      <hr />
+    </div>
+  );
+}
