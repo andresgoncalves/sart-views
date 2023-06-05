52a74fc54a66bb5579747360b4259b5626992249
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2023-06-05 17:38:22 -0400

    InputField done

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import styles from "./InputField.module.scss";
+
 /**
- * @typedef {{
+ * @typedef {React.InputHTMLAttributes<HTMLInputElement> &{
  * labelText: string
+ * placeholder?: string
+ * value?: string
+ * onChange?: function
  * }} InputFieldProps
  * 
  */
 
 /**@param {InputFieldProps} props */
-export default function InputComponent(props) {
-
-  const {labelText} = props;
+export default function InputComponent({labelText,placeholder,...props}) {
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className={styles.container}>
-    <label className={styles.label}>{labelText}</label>
-    <input type="text" className={styles.input} placeholder="Escribe algo"></input>
+    <label className={styles.label}>{labelText}
+    <input type="text" className={styles.input} placeholder={(placeholder)} value= {inputValue} onChange={ev => setInputValue(ev.target.value)}/>
+    </label>
     </div>
   );
 }
