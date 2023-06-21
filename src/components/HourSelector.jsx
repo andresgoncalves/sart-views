56a4dd4f6b8e8289db7a564728102089f6bd9e37
Tomás Gil <tommygilbravo@gmail.com>
2023-06-21 16:54:13 -0400

    Selector de Horas realizado

--- src/components/HourSelector.jsx
+++ src/components/HourSelector.jsx
@@ -1,26 +1,33 @@
 import styles from './HourSelector.module.scss'
+import { useState } from 'react'
 
 function HourSelector(){
+    const [hour, setHour] = useState("10:00am")
+
+    const onOptionChange = e =>{
+        setHour(e.target.value)
+    }
+
     return(
         <>
-        <div className={styles.selectorBox}>
-            <label>                
-                <input type="radio" value="10:00am" name="hour" className={styles.radio}/>
-                10:00am
-            </label>
-            <label>                
-                <input type="radio" value="12:00am" name="hour" className={styles.radio}/>
-                12:00am
-            </label>
-            <label>                
-                <input type="radio" value="2:00pm" name="hour" className={styles.radio}/>
-                2:00pm
-            </label>
-            <label>                
-                <input type="radio" value="4:00pm" name="hour" className={styles.radio}/>
-                4:00pm
-            </label>
-        </div>
+            <div className={styles.selectorBox}>
+                <label className={styles.button}>                
+                    <input type="radio" value="10:00am" name="hour" className={styles.radio} onChange={onOptionChange}/>
+                    <span>10:00am</span>
+                </label>
+                <label className={styles.button}>                
+                    <input type="radio" value="12:00am" name="hour" className={styles.radio} onChange={onOptionChange}/>
+                    <span>12:00am</span>
+                </label>
+                <label className={styles.button}>                
+                    <input type="radio" value="2:00pm" name="hour" className={styles.radio} onChange={onOptionChange}/>
+                    <span>2:00pm</span>
+                </label>
+                <label className={styles.button}>                
+                    <input type="radio" value="4:00pm" name="hour" className={styles.radio} onChange={onOptionChange}/>
+                    <span>4:00pm</span>
+                </label>            
+            </div>
         </>
     )
 }
