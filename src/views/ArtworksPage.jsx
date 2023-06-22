76286b76774e66ab4e702547b62e6ecfba8f4bf3
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-22 17:06:36 -0400

    Fixes

--- src/views/ArtworksPage.jsx
+++ src/views/ArtworksPage.jsx
@@ -1,16 +1,17 @@
-import styles from "./ArtworksPage.module.scss";
 import bannerImage from "../assets/home-banner.png";
+import styles from "./ArtworksPage.module.scss";
 
-export default function ArtworksPage(){
-
-    return(
-        <>
-            <header
-            className={styles.banner}
-            style={{ backgroundImage: `url(${bannerImage})` }}>
-                <div className={styles.contentTitle}>Conoce las obras de arte de la Universidad Metropolitana</div>
-                <div className={styles.}
-            </header>
-        </>
-    );
+export default function ArtworksPage() {
+  return (
+    <>
+      <header
+        className={styles.banner}
+        style={{ backgroundImage: `url(${bannerImage})` }}
+      >
+        <div className={styles.contentTitle}>
+          Conoce las obras de arte de la Universidad Metropolitana
+        </div>
+      </header>
+    </>
+  );
 }
