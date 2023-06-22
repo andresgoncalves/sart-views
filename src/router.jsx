3cf7b67a384d903017e2355b5cb253471027108c
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-06-22 17:53:51 -0400

    Changed routes structure

--- src/router.jsx
+++ src/router.jsx
@@ -10,21 +10,30 @@ import HomePage from "./views/HomePage";
 import LoginPage from "./views/LoginPage";
 import TourProfile from "./views/TourProfile";
 
-
-
 export default createBrowserRouter(
   createRoutesFromElements(
-    <Route errorElement={<ErrorPage />}>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/registro" element={<BlankPage />} />
-        <Route path="/obras" element={<BlankPage />} />
-        <Route path="/tours" element={<BlankPage />} />
-        <Route path="/calendario" element={<BlankPage />} />
-        <Route path="/userDashboard" element={<BlankPage />} />
-        <Route path="/adminDashboard" element={<BlankPage />} />
-        <Route path="/profiletour" element={<TourProfile/>}/>
+    <Route element={<Layout />} errorElement={<ErrorPage />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/registro" element={<BlankPage />} />
+      <Route path="/tours" element={<BlankPage />} />
+      <Route path="/tours/:id" element={<TourProfile />} />
+      <Route path="/obras" element={<BlankPage />} />
+      <Route path="/obras:id" element={<BlankPage />} />
+      <Route path="/calendario" element={<BlankPage />} />
+      <Route path="/buscar" element={<BlankPage />} />
+      <Route path="/user">
+        <Route path="/user/dashboard" element={<BlankPage />} />
+        <Route path="/user/perfil" element={<BlankPage />} />
+      </Route>
+      <Route path="/admin">
+        <Route path="/admin/dashboard" element={<BlankPage />} />
+        <Route path="/admin/obras" element={<BlankPage />} />
+        <Route path="/admin/obras/:id" element={<BlankPage />} />
+        <Route path="/admin/tours" element={<BlankPage />} />
+        <Route path="/admin/tours/:id" element={<BlankPage />} />
+        <Route path="/admin/usuarios" element={<BlankPage />} />
+        <Route path="/admin/usuarios/:id" element={<BlankPage />} />
       </Route>
     </Route>
   )
