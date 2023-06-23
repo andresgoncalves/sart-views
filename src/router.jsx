3565c1da9299e3bd2effe936dfc3d6da4e5478dc
michellevillegasv <michelle.villegas@correo.unimet.edu.ve>
2023-06-22 20:00:22 -0400

    Change ArtworkPage

--- src/router.jsx
+++ src/router.jsx
@@ -9,6 +9,7 @@ import ErrorPage from "./views/ErrorPage";
 import HomePage from "./views/HomePage";
 import LoginPage from "./views/LoginPage";
 import TourProfile from "./views/TourProfile";
+import ArtworksPage from "./views/ArtworksPage";
 
 
 
@@ -19,12 +20,12 @@ export default createBrowserRouter(
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registro" element={<BlankPage />} />
-        <Route path="/obras" element={<BlankPage />} />
         <Route path="/tours" element={<BlankPage />} />
         <Route path="/calendario" element={<BlankPage />} />
         <Route path="/userDashboard" element={<BlankPage />} />
         <Route path="/adminDashboard" element={<BlankPage />} />
         <Route path="/profiletour" element={<TourProfile/>}/>
+        <Route path="/obras" element={<ArtworksPage />}/>
       </Route>
     </Route>
   )
