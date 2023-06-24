24e545f051f3fc2c125485823f383323fcf6d53f
Tomás Gil <tommygilbravo@gmail.com>
2023-06-24 19:55:45 -0400

    Modulo de Selector de horas para las reservas

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,10 +1,30 @@
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
+import HourModal from "../components/HourModal";
 
 export default function ErrorPage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () =>{
+    setModalOpen(true);
+  }
+
+  const closeModal = () =>{
+    setModalOpen(false);
+  };
+
   return (
     <>
       <Helmet title="Error 404" />
       <h1>Página no encontrada 🚨</h1>
+      <button onClick={() => openModal()}>Abrir Modal</button>
+      
+      <button onClick={() => closeModal()}>Cerrar Modal</button>
+      
+
+      {modalOpen && (        
+        <HourModal></HourModal>
+      )}
     </>
   );
 }
