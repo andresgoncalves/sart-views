33c968035685370d69ae268fd9e156c88b940135
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-06-25 00:05:25 -0400

    SearchField Fixes :sparkles:

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,26 +1,37 @@
-import  { useState } from 'react';
-import styles from "./SearchField.module.scss"
+import { useState } from "react";
+import styles from "./SearchField.module.scss";
 
 /**
- * @typedef {React.InputHTMLAttributes<HTMLInputElement> & React.ButtonHTMLAttributes<HTMLInputElement> &{
-* negative?: boolean,
-* placeholder?: string,
-* value?: string,
-* onChange?: function
-* }} SearchFieldProps
-*/
+ * @typedef {React.InputHTMLAttributes<HTMLInputElement> &
+ *   React.ButtonHTMLAttributes<HTMLInputElement> & {
+ *     negative?: boolean;
+ *     placeholder?: string;
+ *     value?: string;
+ *     onChange?: function;
+ *   }} SearchFieldProps
+ */
 
-
-/**@param {SearchFieldProps} props */
-export default function SearchField({placeholder, value, negative = false, ...props})  {
-
-  const [searchValue, setSearchValue] = useState('');
+/** @param {SearchFieldProps} props */
+export default function SearchField({
+  placeholder,
+  value,
+  negative = false,
+  ...props
+}) {
+  const [searchValue, setSearchValue] = useState("");
 
   return (
-    <div className={[styles.container, negative? styles.negative : ""].join(" ")}>
-    <input type="text" className={styles.input} placeholder="Buscar" value= {searchValue} onChange={ev => setSearchValue(ev.target.value)}/>
-    <button>Buscar</button>
+    <div
+      className={[styles.container, negative ? styles.negative : ""].join(" ")}
+    >
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Buscar"
+        value={searchValue}
+        onChange={(ev) => setSearchValue(ev.target.value)}
+      />
+      <button className={styles.button}>Buscar</button>
     </div>
   );
-};
-
+}
