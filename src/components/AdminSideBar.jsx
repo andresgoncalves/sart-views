2a84ac342598a0436ca87ca82473b4d80b1ee81e
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-06-26 09:38:08 -0400

    Adding admin headers :sparkles:

---
+++ src/components/AdminSideBar.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import editLapiz from "../assets/EditLapiz.svg";
+import exitIcon from "../assets/exitIcon.svg";
+import styles from "./AdminSideBar.module.scss";
+
+export default function AdminSideBar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.photo}>
+          <img></img>
+        </div>
+        <div className={styles.adminName}>Jack Mason</div>
+        <Link to="/" className={styles.edit}>
+          Editar Perfil
+          <img src={editLapiz}></img>
+        </Link>
+      </div>
+      <Link to="/" className={styles.option}>
+        Dashboard
+      </Link>
+      <Link to="/" className={styles.option}>
+        Obras
+      </Link>
+      <Link to="/" className={styles.option}>
+        Tours
+      </Link>
+      <Link to="/" className={styles.option}>
+        Usuarios
+      </Link>
+      <Link to="/" className={[styles.optionSpe, styles.close].join(" ")}>
+        Cerrar Sesión <img src={exitIcon}></img>
+      </Link>
+    </div>
+  );
+}
---
+++ src/views/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import AdminHeader from "../components/AdminHeader";
+import AdminSideBar from "../components/AdminSideBar";
+
+export default function AdminDashboard() {
+  return (
+    <div>
+      <AdminHeader></AdminHeader>
+      <AdminSideBar></AdminSideBar>
+    </div>
+  );
+}
