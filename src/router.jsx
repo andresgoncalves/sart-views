a3cb782ce6215f1565af2e86949dd14078b7cc3c
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-06-26 14:43:08 -0400

    Admin Layout Implemented :sparkles:

--- src/router.jsx
+++ src/router.jsx
@@ -4,10 +4,12 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "./components/Layout";
+import LayoutAdmin from "./components/LayoutAdmin";
 import { PrivateRoutePublic } from "./components/PrivateRoutePublic";
 import { PrivateRouteUser } from "./components/PrivateRouteUser";
 import AdminArtworkPage from "./views/AdminArtworkPage";
 import AdminArtworksPage from "./views/AdminArtworksPage";
+import AdminDashboard from "./views/AdminDashboard";
 import AdminTourPage from "./views/AdminTourPage";
 import AdminToursPage from "./views/AdminToursPage";
 import ArtworksPage from "./views/ArtworksPage";
@@ -23,64 +25,70 @@ import ToursPage from "./views/ToursPage";
 
 export default createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<Layout />} errorElement={<ErrorPage />}>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/login"
-        element={
-          <PrivateRoutePublic>
-            <LoginPage />
-          </PrivateRoutePublic>
-        }
-      />
-      <Route path="/registro" element={<BlankPage />} />
-      <Route path="/calendario" element={<CalendarPage />} />
-      <Route path="/buscar" element={<SearchPage />} />
-      <Route path="/obras">
-        <Route path="/obras" element={<ArtworksPage />} />
-        <Route path="/obras/:id" element={<BlankPage />} />
-      </Route>
-      <Route path="/tours">
-        <Route path="/tours" element={<ToursPage />} />
-        <Route path="/tours/:id" element={<TourProfile />} />
+    <>
+      <Route element={<Layout />} errorElement={<ErrorPage />}>
+        <Route path="/" element={<HomePage />} />
         <Route
-          path="/tours/:id/reservar"
+          path="/login"
           element={
-            <PrivateRouteUser>
-              <TourProfile />
-            </PrivateRouteUser>
+            <PrivateRoutePublic>
+              <LoginPage />
+            </PrivateRoutePublic>
           }
         />
+        <Route path="/registro" element={<BlankPage />} />
+        <Route path="/calendario" element={<CalendarPage />} />
+        <Route path="/buscar" element={<SearchPage />} />
+        <Route path="/obras">
+          <Route path="/obras" element={<ArtworksPage />} />
+          <Route path="/obras/:id" element={<BlankPage />} />
+        </Route>
+        <Route path="/tours">
+          <Route path="/tours" element={<ToursPage />} />
+          <Route path="/tours/:id" element={<TourProfile />} />
+          <Route
+            path="/tours/:id/reservar"
+            element={
+              <PrivateRouteUser>
+                <TourProfile />
+              </PrivateRouteUser>
+            }
+          />
+        </Route>
+        <Route path="/user">
+          <Route
+            path="/user/dashboard"
+            element={
+              <PrivateRouteUser>
+                <ProfileUser />
+              </PrivateRouteUser>
+            }
+          />
+          <Route
+            path="/user/perfil"
+            element={
+              <PrivateRouteUser>
+                <BlankPage />
+              </PrivateRouteUser>
+            }
+          />
+        </Route>
+        <Route path="/admin">
+          <Route path="/admin/obras" element={<AdminArtworksPage />} />
+          <Route path="/admin/obras/:id" element={<AdminArtworkPage />} />
+          <Route path="/admin/obras/crear" element={<AdminArtworkPage />} />
+          <Route path="/admin/tours" element={<AdminToursPage />} />
+          <Route path="/admin/tours/:id" element={<AdminTourPage />} />
+          <Route path="/admin/tours/crear" element={<AdminTourPage />} />
+          <Route path="/admin/usuarios" element={<BlankPage />} />
+          <Route path="/admin/usuarios/:id" element={<BlankPage />} />
+        </Route>
       </Route>
-      <Route path="/user">
-        <Route
-          path="/user/dashboard"
-          element={
-            <PrivateRouteUser>
-              <ProfileUser />
-            </PrivateRouteUser>
-          }
-        />
-        <Route
-          path="/user/perfil"
-          element={
-            <PrivateRouteUser>
-              <BlankPage />
-            </PrivateRouteUser>
-          }
-        />
+      <Route element={<LayoutAdmin />} errorElement={<ErrorPage />}>
+        <Route path="/admin">
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        </Route>
       </Route>
-      <Route path="/admin">
-        <Route path="/admin/dashboard" element={<BlankPage />} />
-        <Route path="/admin/obras" element={<AdminArtworksPage />} />
-        <Route path="/admin/obras/:id" element={<AdminArtworkPage />} />
-        <Route path="/admin/obras/crear" element={<AdminArtworkPage />} />
-        <Route path="/admin/tours" element={<AdminToursPage />} />
-        <Route path="/admin/tours/:id" element={<AdminTourPage />} />
-        <Route path="/admin/tours/crear" element={<AdminTourPage />} />
-        <Route path="/admin/usuarios" element={<BlankPage />} />
-        <Route path="/admin/usuarios/:id" element={<BlankPage />} />
-      </Route>
-    </Route>
+    </>
   )
 );
