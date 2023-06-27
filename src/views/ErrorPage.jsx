c01979d64b842d442667fca99ab850f419a07441
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2023-06-27 12:09:39 -0400

    Little changes :wrench:

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -7,10 +7,6 @@ import PaypalModal from "../components/PaypalModal";
 export default function ErrorPage() {
   const [image, setImage] = useState("");
 
-     const [modalOpen, setModalOpen] = useState(false);
-
-   const openModal = () => setModalOpen(true);
-   const closeModal = () => setModalOpen(false);
 
   return (
     <>
