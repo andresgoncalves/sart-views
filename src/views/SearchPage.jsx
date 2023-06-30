e1063c70a06cd5aca600be584fddf2471daacf9b
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2023-06-30 18:34:13 -0400

    Searching done :rocket:

--- src/views/SearchPage.jsx
+++ src/views/SearchPage.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import ArtworkCard from "../components/ArtworkCard";
 import SearchField from "../components/SearchField";
@@ -11,6 +11,8 @@ import styles from "./SearchPage.module.scss";
 export default function SearchPage() {
   const artworks = useArtworks();
   const tours = useTours();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const tags = [
     {
@@ -34,32 +36,41 @@ export default function SearchPage() {
   ];
 
   const results = useMemo(
-    () =>
-      tours.data && artworks.data
-        ? [
-            ...tours.data.map((data) => ({
-              /** @type {"tour"} */
-              type: "tour",
-              data,
-            })),
-            ...artworks.data.map((data) => ({
-              /** @type {"artwork"} */
-              type: "artwork",
-              data,
-            })),
-          ]
-            .sort(() => Math.random() - 0.5)
-            .slice(0, 8)
-        : [],
-    [artworks.data, tours.data]
+    () => {
+      if (!tours.data || !artworks.data) return [];
+  
+      const filteredTours = tours.data.filter((data) =>
+        data.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      const filteredArtworks = artworks.data.filter((data) =>
+        data.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+  
+      return [
+        ...filteredTours.map((data) => ({
+          /** @type {"tour"} */
+          type: "tour",
+          data,
+        })),
+        ...filteredArtworks.map((data) => ({
+          /** @type {"artwork"} */
+          type: "artwork",
+          data,
+        })),
+      ]
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 8);
+    },
+    [artworks.data, tours.data, searchQuery]
   );
+  
 
   return (
     <>
       <Helmet title="Buscador"></Helmet>
       <div className={styles.container}>
         <div className={styles.searchField}>
-          <SearchField />
+          <SearchField action={setSearchQuery} />
         </div>
         <div className={styles.tagsContainer}>
           <div className={styles.labelCategory}>Categorías:</div>
