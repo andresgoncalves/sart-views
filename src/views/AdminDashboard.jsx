07c7817f9ff59033e9ba01f74ae36861b3553360
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2023-07-02 23:47:10 -0400

    Dashboard fix

--- src/views/AdminDashboard.jsx
+++ src/views/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import { Helmet } from "react-helmet-async";
 import logo from "../assets/LogoSartViews.svg";
 import AdminDashboardHeader from "../components/AdminDashboardHeader";
+import ArtworksGrid from "../components/ArtworksGrid";
 import Button from "../components/Button";
+import DetailedToursGrid from "../components/DetailedToursGrid";
 import Divider from "../components/Divider";
 import Loader from "../components/Loader";
 import ToursGrid from "../components/ToursGrid";
 import { useArtworks } from "../hooks/artworks";
-import { useReservations } from "../hooks/reservations";
+import { useUpcomingReservations } from "../hooks/reservations";
 import { useTours } from "../hooks/tours";
 import { useUsers } from "../hooks/users";
 import styles from "./AdminDashboard.module.scss";
@@ -15,7 +17,7 @@ export default function AdminDashboard() {
   const tours = useTours();
   const artworks = useArtworks();
   const users = useUsers();
-  const reservations = useReservations();
+  const reservations = useUpcomingReservations(6);
 
   return (
     <>
@@ -56,25 +58,40 @@ export default function AdminDashboard() {
         <Divider>
           <div className={styles.titleLabel}>Próximos Eventos</div>
         </Divider>
-        <div className={styles.nextTours}>
-          <Button>Crear Evento</Button>
+        <div className={styles.tourContainer}>
+          <DetailedToursGrid
+            tours={tours.data?.slice(0, 6).map((tour) => ({
+              ...tour,
+              status: "unavailable",
+            }))}
+            fallback="No hay tours registrados"
+          />
         </div>
       </section>
       <section>
         <div className={styles.dataAdmin}>
-          <div className={styles.column1}>
+          <div className={styles.column}>
             <div className={styles.container}>
-              <div className={styles.titleLabel}>Tours Populares</div>
-              <Button>Agregar Tour</Button>
+              <div className={styles.titleLabel}>Tours populares</div>
+              <Button href="/admin/tours/crear">Agregar Tour</Button>
             </div>
             <div className={styles.tourContainer}>
-              <ToursGrid tours={tours.data} fallback="No hay tours visitados" />
+              <ToursGrid
+                tours={tours.data?.slice(0, 8)}
+                fallback="No hay tours registrados"
+              />
             </div>
           </div>
-          <div className={styles.column2}>
+          <div className={styles.column}>
             <div className={styles.container}>
-              <div className={styles.titleLabel}>Obras Destacadas</div>
-              <Button>Agregar Obra</Button>
+              <div className={styles.titleLabel}>Obras destacadas</div>
+              <Button href="/admin/obras/crear">Agregar Obra</Button>
+            </div>
+            <div className={styles.tourContainer}>
+              <ArtworksGrid
+                artworks={artworks.data?.slice(0, 12)}
+                fallback="No hay obras registradas"
+              />
             </div>
           </div>
         </div>
