cf69a5bf79487baa2ebd713323481f3165878ffe
Tomás Gil <tommygilbravo@gmail.com>
2023-07-02 22:59:04 -0400

    Limpieza Pag de Error

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
 import { Helmet } from "react-helmet-async";
-import DonateModal from "../components/DonateModal";
-import ReserveModal from "../components/ReserveModal";
 
 export default function ErrorPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
   return (
     <>
       <Helmet title="Error 404" />
       <h1>Página no encontrada 🚨</h1>
-
-      <button onClick={openModal}>Open Modal</button>
-      {isModalOpen && <ReserveModal closeModal={() => setIsModalOpen(false)} />}
     </>
   );
 }
