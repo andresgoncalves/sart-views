0877f696e3104c731e8631673fb60d29eefcfab1
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-07-03 00:00:16 -0400

    Fix

--- src/views/AdminDashboard.jsx
+++ src/views/AdminDashboard.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Helmet } from "react-helmet-async";
 import logo from "../assets/LogoSartViews.svg";
 import AdminDashboardHeader from "../components/AdminDashboardHeader";
@@ -7,6 +8,7 @@ import DetailedToursGrid from "../components/DetailedToursGrid";
 import Divider from "../components/Divider";
 import Loader from "../components/Loader";
 import ToursGrid from "../components/ToursGrid";
+import { useAuth } from "../contexts/AuthContext";
 import { useArtworks } from "../hooks/artworks";
 import { useUpcomingReservations } from "../hooks/reservations";
 import { useTours } from "../hooks/tours";
@@ -15,9 +17,16 @@ import styles from "./AdminDashboard.module.scss";
 
 export default function AdminDashboard() {
   const tours = useTours();
-  const artworks = useArtworks();
-  const users = useUsers();
+  const users = useUsers(null);
+  const { user } = useAuth();
+  const artworksCount = useArtworks();
+  const artworks = useArtworks(user?.favoritesArtworks || []);
   const reservations = useUpcomingReservations(6);
+  const upcomingTours = useMemo(
+    () => reservations.data?.map((reservation) => reservation.tour) || [],
+    [reservations.data]
+  );
+  const toursNext = useTours(upcomingTours);
 
   return (
     <>
@@ -49,49 +58,47 @@ export default function AdminDashboard() {
               <Loader />
             )}
           </div>
-          <div className={styles.contentBox}>
-            <div className={styles.label}>Fondos Recaudados</div>
-          </div>
         </div>
       </section>
       <section>
         <Divider>
           <div className={styles.titleLabel}>Próximos Eventos</div>
         </Divider>
-        <div className={styles.tourContainer}>
-          <DetailedToursGrid
-            tours={tours.data?.slice(0, 6).map((tour) => ({
-              ...tour,
-              status: "unavailable",
-            }))}
-            fallback="No hay tours registrados"
-          />
+        <div className={styles.nextContainer}>
+          <DetailedToursGrid tours={toursNext.data} size="base" />
+        </div>
+        <div className={styles.nextTours}>
+          <Button className={styles.create}>Crear Evento</Button>
         </div>
       </section>
       <section>
+        <Divider></Divider>
         <div className={styles.dataAdmin}>
-          <div className={styles.column}>
+          <div className={styles.column1}>
             <div className={styles.container}>
-              <div className={styles.titleLabel}>Tours populares</div>
-              <Button href="/admin/tours/crear">Agregar Tour</Button>
+              <div className={styles.titleLabel}>Tours Populares</div>
+              <Button>Agregar Tour</Button>
             </div>
             <div className={styles.tourContainer}>
-              <ToursGrid
-                tours={tours.data?.slice(0, 8)}
-                fallback="No hay tours registrados"
-              />
+              <ToursGrid tours={tours.data} fallback="No hay tours visitados" />
             </div>
           </div>
-          <div className={styles.column}>
+          <div className={styles.column2}>
             <div className={styles.container}>
-              <div className={styles.titleLabel}>Obras destacadas</div>
-              <Button href="/admin/obras/crear">Agregar Obra</Button>
+              <div className={styles.titleLabel}>Obras Destacadas</div>
+              <Button>Agregar Obra</Button>
             </div>
             <div className={styles.tourContainer}>
-              <ArtworksGrid
-                artworks={artworks.data?.slice(0, 12)}
-                fallback="No hay obras registradas"
-              />
+              {artworks.data ? (
+                <ArtworksGrid
+                  artworks={artworks.data}
+                  fallback="No hay obras destacadas"
+                />
+              ) : (
+                <div>
+                  <Loader></Loader>
+                </div>
+              )}
             </div>
           </div>
         </div>
